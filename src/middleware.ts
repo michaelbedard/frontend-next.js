443c27f5c37ebc9b48aa5d6d4f66d83c69c74e99
michaelbedard <courriermichaelbedard@gmail.com>
2023-11-24 12:09:44 -0500

    added google analytics #7

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,37 +1,40 @@
 import {NextRequest, NextResponse} from "next/server";
 import { withAuth, NextRequestWithAuth } from "next-auth/middleware";
 
-export default withAuth(
-    function middleware(request : NextRequestWithAuth) {
-        console.log("MIDDLEWARE WITH AUTH")
-
-        if (request.nextUrl.pathname == '/') {
-            return NextResponse.rewrite(
-                new URL("/home", request.url)
-            )
-        }
-        if (request.nextUrl.pathname.startsWith("/auth/admin")
-        && request.nextauth.token?.role !== "ADMIN") {
-            return NextResponse.rewrite(
-                new URL("/denied", request.url)
-            )
-        }
-        if (request.nextUrl.pathname.startsWith("/auth/user")
-        && request.nextauth.token?.role !== "ADMIN"
-            && request.nextauth.token?.role !== "MANAGER" ) {
-            new URL("/denied", request.url)
-        }
-
-    },
-    {
-        callbacks : {
-            authorized: () => true
-        }
-    }
-)
-
-export const config = {
-    matcher: [
-        '/((?!api|_next/static|_next/image|favicon.ico).*)'
-    ]
+export default function middleware(request : NextRequest) {
+    console.log("MIDDLEWARE")
 }
+// export default withAuth(
+//     function middleware(request : NextRequestWithAuth) {
+//         console.log("MIDDLEWARE WITH AUTH")
+//
+//         if (request.nextUrl.pathname == '/') {
+//             return NextResponse.rewrite(
+//                 new URL("/home", request.url)
+//             )
+//         }
+//         if (request.nextUrl.pathname.startsWith("/auth/admin")
+//         && request.nextauth.token?.role !== "ADMIN") {
+//             return NextResponse.rewrite(
+//                 new URL("/denied", request.url)
+//             )
+//         }
+//         if (request.nextUrl.pathname.startsWith("/auth/user")
+//         && request.nextauth.token?.role !== "ADMIN"
+//             && request.nextauth.token?.role !== "MANAGER" ) {
+//             new URL("/denied", request.url)
+//         }
+//
+//     },
+//     {
+//         callbacks : {
+//             authorized: () => true
+//         }
+//     }
+// )
+//
+// export const config = {
+//     matcher: [
+//         '/((?!api|_next/static|_next/image|favicon.ico).*)'
+//     ]
+// }
